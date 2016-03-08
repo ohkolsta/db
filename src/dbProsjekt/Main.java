76494dcb5721d56b7752ba742ce8634ebcb51b9c
ohkolsta <ohkolsta@stud.ntnu.no>
2016-03-08 14:25:05 +0100

    Scanner in Main

--- src/dbProsjekt/Main.java
+++ src/dbProsjekt/Main.java
@@ -1,5 +1,29 @@
 package dbProsjekt;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	String intro = "Hei og velkommen til Treningsdagbok!";
+	
+	public void Main(){
+	}
+	
+	public void printIntro(){
+		System.out.println(intro);
+	}
+	
+	public void useScanner(){
+		Scanner sc = new Scanner(System.in);
+		
+	}
+	
+	public static void main(String args[]){
+		Main main = new Main();
+		main.printIntro();
+		main.useScanner();
+		
+		
+	}
 
 }
