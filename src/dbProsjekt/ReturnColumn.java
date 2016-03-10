3d08d806c9e4baa94b23f7f4d0e27757410ec9af
ohkolsta <ohkolsta@stud.ntnu.no>
2016-03-10 15:19:20 +0100

    worked on ReturnColumn

--- src/dbProsjekt/ReturnColumn.java
+++ src/dbProsjekt/ReturnColumn.java
@@ -1,6 +1,7 @@
 package dbProsjekt;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -25,15 +26,17 @@ public class ReturnColumn {
 		}
 	}
 	
-	public void listOkter() throws Exception{
+	public void listOkter(String oktNumber) throws Exception{
 		Kobling kobling = new Kobling();
 		Connection conn = kobling.getConnection();
 		ResultSet rs = null;
 		Statement st = null;
+		int t = 0;
+		int oktnr = Integer.parseInt(oktNumber);
 		try{
 			String k = "";
 			st = conn.createStatement();
-			rs = st.executeQuery("SELECT * FROM Okt INNER JOIN OktOvelser ON (Okt.OktId = OktOvelser.OktId)");
+			rs = st.executeQuery("SELECT * FROM Okt INNER JOIN OktOvelser ON ((Okt.OktId = OktOvelser.OktId) AND (Okt.OktId = '"+oktNumber+"'))");
 			if (rs.next()) {
 				k = rs.getString(1) + " | " + rs.getString(2) + " | " + rs.getString(3);
 				System.out.println(k);
@@ -44,9 +47,27 @@ public class ReturnColumn {
 		try{
 			String k = "";
 			st = conn.createStatement();
-			rs = st.executeQuery("SELECT * FROM Okt INNER JOIN OktOvelser ON (Okt.OktId = OktOvelser.OktId)");
+			rs = st.executeQuery("SELECT * FROM Okt INNER JOIN OktOvelser ON ((Okt.OktId = OktOvelser.OktId) AND (Okt.OktId = '"+oktNumber+"'))");
 			while (rs.next()) {
 				k = rs.getString(5);
+				System.out.println(k);
+			}
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void listAllOkter() throws Exception{
+		Kobling kobling = new Kobling();
+		Connection conn = kobling.getConnection();
+		ResultSet rs = null;
+		Statement st = null;
+		try{
+			String k = "";
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM Okt");
+			while (rs.next()) {
+				k = rs.getString(1) + " | " + rs.getString(2) + " | " + rs.getString(3);
 				System.out.println(k);
 			}
 		} catch(Exception e) {
@@ -58,7 +79,7 @@ public class ReturnColumn {
 	
 	public static void main(String[] args) throws Exception {
 		ReturnColumn rc = new ReturnColumn();
-		rc.listOkter();
+		rc.listOkter("2");
 	}
 }
 
